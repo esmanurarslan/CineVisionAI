377dec69100b8ae56d6b0f8c6265a4b4b95f4d77
Esma Nur Arslan <96125581+esmanurarslan@users.noreply.github.com>
2025-03-21 16:35:04 +0300

    Initial Commit

---
+++ CineVisionAI/CineVisionAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineVisionAIApp.swift
+//  CineVisionAI
+//
+//  Created by Esma Nur Arslan on 21.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CineVisionAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

6958e58abb17c6e4c409d93ecc4bd4fd952468ec
Esma Nur Arslan <96125581+esmanurarslan@users.noreply.github.com>
2025-03-22 00:46:14 +0300

    SignUpView created

---
+++ CineVisionAI/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  CineVisionAI
+//
+//  Created by Esma Nur Arslan on 21.03.2025.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}

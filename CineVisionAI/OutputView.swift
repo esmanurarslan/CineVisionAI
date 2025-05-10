db649b82357d27a40cb10f653243af7ac4271d5c
Esma Nur Arslan <96125581+esmanurarslan@users.noreply.github.com>
2025-05-10 23:51:53 +0300

    OutputView Screen and fix design

---
+++ CineVisionAI/OutputView.swift
@@ -0,0 +1,18 @@
+//
+//  OutputView.swift
+//  CineVisionAI
+//
+//  Created by Esma Nur Arslan on 10.05.2025.
+//
+
+import SwiftUI
+
+struct OutputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OutputView()
+}
---
+++ CineVisionAI/RootViewManager.swift
@@ -0,0 +1,8 @@
+//
+//  RootViewManager.swift
+//  CineVisionAI
+//
+//  Created by Esma Nur Arslan on 10.05.2025.
+//
+
+import Foundation
